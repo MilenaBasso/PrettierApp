7ce62e953534daf5570ad3032bd77a766263cd75
Milena <milena.basso444@gmail.com>
2024-03-13 14:07:12 -0300

    buttons OK

--- src/components/BtnComponent.js
+++ src/components/BtnComponent.js
@@ -5,15 +5,15 @@ import { styles } from "../styles/StyleSheet";
 export default function BtnComponent() {
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={() => console.log("Actually, I don't have a login page:(")}>
         <Text>LOG IN</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={() => console.log("U can't sign in here...")}>
         <Text>SIGN IN</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={styles.btn}>
+      <TouchableOpacity style={styles.btn} onPress={() => console.log("Do u like cats??:D")}>
         <Text>IMAGE</Text>
       </TouchableOpacity>
       <Image>
