c714d36f8edef7394e1caf67ea21bbfe7a63f893
Milena <milena.basso444@gmail.com>
2024-03-13 14:03:33 -0300

    erro fixed

--- src/components/BtnComponent.js
+++ src/components/BtnComponent.js
@@ -1,19 +1,19 @@
 import { TouchableOpacity } from "react-native-web";
-import { View, Image } from "react-native";
+import { View, Image, Text } from "react-native";
 import { styles } from "../styles/StyleSheet";
 
 export default function BtnComponent() {
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={style.btn}>
+      <TouchableOpacity style={styles.btn}>
         <Text>LOG IN</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={style.btn}>
+      <TouchableOpacity style={styles.btn}>
         <Text>SIGN IN</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity style={style.btn}>
+      <TouchableOpacity style={styles.btn}>
         <Text>IMAGE</Text>
       </TouchableOpacity>
       <Image>
